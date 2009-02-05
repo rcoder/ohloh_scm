839751ea2ac3e85063c8bbcb78366090d0bf4f18
Robin Luckey <robin@ohloh.net>
2009-02-05 15:26:30 -0800

    [NEW] BzrParser now accepts revision-ids in preferences to revision numbers if ids are present.       BzrParser now accepts the long format with either or both the -v and --show-id options.

--- lib/scm/parsers/bzr_parser.rb
+++ lib/scm/parsers/bzr_parser.rb
@@ -9,6 +9,7 @@ module Scm::Parsers
 			e = nil
 			state = :data
 			action = ''
+			show_id = false # true if this log includes revision and file ids
 
 			buffer.each_line do |l|
 				next_state = state
@@ -22,6 +23,10 @@ module Scm::Parsers
 					next_state = :data
 				when /^revno:\s+(\d+)$/
 					e.token = $1
+					next_state = :data
+				when /^revision-id:\s+(\S+)$/
+					e.token = $1
+					show_id = true
 					next_state = :data
 				when /^committer:\s+(.+?)(\s+<(.+)>)?$/
 					e.committer_name = $1
@@ -43,9 +48,13 @@ module Scm::Parsers
 					next_state = :collect_message
 					e.message ||= ''
 				when /^  (.*)$/
-					if state == :collect_files and $1.length > 0
-						e.diffs << Scm::Diff.new(:action => action, :path => $1)
-					elsif state == :collect_message
+					case state
+					when :collect_files
+						path = $1
+						# strip the id from the filename if it is present
+						path = $1 if show_id && path =~ /^(.+?)\s+(\S+)$/
+						e.diffs << Scm::Diff.new(:action => action, :path => path)
+					when :collect_message
 						e.message << $1
 						e.message << "\n"
 					end
