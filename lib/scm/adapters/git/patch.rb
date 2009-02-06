984b43ab8d012d2187f31baef8e704533adbf6c4
Lennon Day-Reynolds <lennon.dayreynolds@sun.com>
2009-02-06 15:35:36 -0800

    fix ordering of revisions in call to git-diff

--- lib/scm/adapters/git/patch.rb
+++ lib/scm/adapters/git/patch.rb
@@ -1,8 +1,8 @@
 module Scm::Adapters
   class GitAdapter < AbstractAdapter
     def patch_for_commit(commit)
-      parents_tokens(commit).map {|token|
-        run("cd #{url} && git diff #{commit.token} #{token}")
+      parent_tokens(commit).map {|token|
+        run("cd #{url} && git diff #{token} #{commit.token}")
       }.join("\n")
     end
   end
