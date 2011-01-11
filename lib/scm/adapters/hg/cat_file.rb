e5a6ea9eb321b17892c7541bf297d734701b3d0f
Robin Luckey <robinluckey@gmail.com>
2011-01-11 12:21:03 -0800

    OTWO-171 Fixes hg filenames that include '#' char
    Rolls back 6851c5ca and applies alternate fix. Unit tests now pass.

--- lib/scm/adapters/hg/cat_file.rb
+++ lib/scm/adapters/hg/cat_file.rb
@@ -10,7 +10,7 @@ module Scm::Adapters
 		end
 
 		def cat(revision, path)
-			out, err = run_with_err("cd '#{url}' && hg cat -r #{revision} '#{escape(path)}'")
+			out, err = run_with_err("cd '#{url}' && hg cat -r #{revision} #{escape(path)}")
 			return nil if err =~ /No such file in rev/
 			raise RuntimeError.new(err) unless err.to_s == ''
 			out
@@ -20,7 +20,7 @@ module Scm::Adapters
 		# Example:
 		#     "Foo Bar & Baz" => "Foo\ Bar\ \&\ Baz"
 		def escape(path)
-			path.gsub(/[ '"&()<>|]/) { |c| '\\' + c }
+			path.gsub(/[ '"&()<>|#]/) { |c| '\\' + c }
 		end
 	end
 end
