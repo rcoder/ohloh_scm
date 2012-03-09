68b9ecc722336eeceefa4f6887007a2b9e5d3fb1
Robin Luckey <rluckey@blackducksoftware.com>
2012-03-08 19:25:08 -0800

    OTWO-1412 Upgrades Bzr repositories after every pull
    We can't know when it's required, but it's fatal to miss it,
    So we force a `bzr upgrade` after every pull.

--- lib/scm/adapters/bzr/pull.rb
+++ lib/scm/adapters/bzr/pull.rb
@@ -11,12 +11,28 @@ module Scm::Adapters
 				run "mkdir -p '#{self.url}'"
 				run "rm -rf '#{self.url}'"
 				run "bzr branch '#{from.url}' '#{self.url}'"
+        clean_up_disk
 			else
 				run "cd '#{self.url}' && bzr revert && bzr pull --overwrite '#{from.url}'"
 			end
 
 			yield(1,1) if block_given? # Progress bar callback
 		end
+
+    def clean_up_disk
+      # Usually a `bzr upgrade` is unnecessary, but it's fatal to miss it when
+      # required. Because I don't know how to detect in advance whether we
+      # actually need it, we play it safe and always upgrade.
+      #
+      # Unfortunately, not only can we not know whether it is required,
+      # but the command fails and raises when the upgrade is not required.
+
+      begin
+			  run "cd '#{self.url}' && bzr upgrade"
+      rescue RuntimeError => e
+        raise unless e.message =~ /already at the most recent format/
+      end
+    end
 
 	end
 end
