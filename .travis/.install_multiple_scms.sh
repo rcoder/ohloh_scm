8346b299b9c2d04ece579e36bd4f558df8ae0bdf
arun <aruncsengr@gmail.com>
2016-06-22 16:38:37 +0530

    OTWO-4281 Configured travis ssh setup and its dependencies

--- .travis/.install_multiple_scms.sh
+++ .travis/.install_multiple_scms.sh
@@ -1,8 +1,5 @@
-sudo apt-get install -y cvs
-sudo ln -s /usr/bin/cvs /usr/bin/cvsnt
-sudo apt-get install -y bzr
-sudo apt-get install -y mercurial
 sudo sh -c 'echo "deb http://opensource.wandisco.com/ubuntu precise svn18" >> /etc/apt/sources.list.d/subversion18.list'
 sudo wget -q http://opensource.wandisco.com/wandisco-debian.gpg -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y subversion
+sudo apt-get install -y subversion cvs bzr mercurial
+sudo ln -s /usr/bin/cvs /usr/bin/cvsnt
