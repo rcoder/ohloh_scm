c7c675de27595d67927aa2cf0b0bd386347b29f7
Priya Rani <prani@blackducksoftware.com>
2018-01-03 17:58:25 +0530

    OTWO-5068 Remove mercurial installed by pip, remove precise distribution from travis.yml and remove svn18 from install_multiple_scms sh file

--- .travis/.install_multiple_scms.sh
+++ .travis/.install_multiple_scms.sh
@@ -1,6 +1,4 @@
-sudo sh -c 'echo "deb http://opensource.wandisco.com/ubuntu precise svn18" >> /etc/apt/sources.list.d/subversion18.list'
-sudo wget -q http://opensource.wandisco.com/wandisco-debian.gpg -O- | sudo apt-key add -
-sudo apt-add-repository -y ppa:git-core/ppa
 sudo apt-get update
+sudo pip uninstall mercurial -y
 sudo apt-get install -y git subversion cvs bzr mercurial
 sudo ln -s /usr/bin/cvs /usr/bin/cvsnt
