1add34dd89649fd31c5dc99ba47b439a348c9eed
Alex Johnson <notalexjohnson@gmail.com>
2019-04-15 14:08:33 +0530

    Fix test failures

--- .travis/.install_multiple_scms.sh
+++ .travis/.install_multiple_scms.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo pip uninstall mercurial -y
-sudo apt-get install -y git subversion cvs bzr mercurial
+sudo apt-get install -y git subversion cvs bzr mercurial expect
 sudo ln -s /usr/bin/cvs /usr/bin/cvsnt
 
 sudo apt-get install git-svn
