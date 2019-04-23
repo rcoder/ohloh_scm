f62e1862a26ef925767f10944b97f68b49e8a182
Alex Johnson <notalexjohnson@gmail.com>
2019-04-23 19:26:00 +0530

    Remove all files for rewrite

--- lib/ohloh_scm/adapters/bzrlib/cat_file.rb
+++
@@ -1,9 +0,0 @@
-module OhlohScm::Adapters
-	class BzrlibAdapter < BzrAdapter
-
-		def cat(revision, path)
-      content = bzr_client.cat_file(revision, path)
-		end
-
-	end
-end
--- lib/ohloh_scm/version.rb
+++
@@ -1,5 +0,0 @@
-module OhlohScm
-  module Version
-    STRING = '2.5.0'
-  end
-end
