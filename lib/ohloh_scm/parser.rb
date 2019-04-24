2541bc0fbe3869d0643d835a288e3c5471b38edd
Alex Johnson <notalexjohnson@gmail.com>
2019-04-24 16:51:33 +0530

    Move Parser and related classes

---
+++ lib/ohloh_scm/parser.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative 'parser/array_writer'
+
+module OhlohScm
+  class Parser
+    class << self
+      def parse(buffer = '', opts = {})
+        buffer = StringIO.new(buffer) if buffer.is_a?(String)
+        writer = ArrayWriter.new unless block_given?
+
+        internal_parse(buffer, opts) do |commit|
+          if commit
+            yield commit if block_given?
+            writer&.write_commit(commit)
+          end
+        end
+
+        writer&.buffer
+      end
+
+      def internal_parse; end
+    end
+  end
+end
