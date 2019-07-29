56b337dcdda6e071422e84a6d0a54702e6daca0d
kselvraj <kselvraj@blackduck.com>
2019-07-29 18:21:17 +0530

    Leverage OhlohScm environment variable to pass temp folder option

--- spec/ohloh_scm/activity_spec.rb
+++ spec/ohloh_scm/activity_spec.rb
@@ -11,10 +11,11 @@ describe 'Activity' do
     end
 
     it 'should return temp folder path' do
+      ENV['OHLOH_SCM_TEMP_FOLDER_PATH'] = '/test'
       core = get_core(:git)
-      core.temp_dir = '/test'
       scm = OhlohScm::Activity.new(core)
       scm.log_filename.must_equal '/test/foobar.log'
+      ENV['OHLOH_SCM_TEMP_FOLDER_PATH'] = ''
     end
   end
 end
